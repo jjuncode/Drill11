cc9e7111277e0f10118421dde243c87463fefdb6
jjuncode <jjun.code@gmail.com>
2023-11-01 19:09:40 +0900

    bird move fly + get direction

--- bird.py
+++ bird.py
@@ -18,10 +18,11 @@ MAX_FRAME = [4,5,5]
 
 FRAMES_PER_TIME = ACTION_PER_TIME * FRAMES_PER_ACTION
 
+BIRD_SIZE = [200,200]
 
 class Bird:
     def __init__(self):
-        self.x, self.y = 400, 90
+        self.x, self.y = 400, 500
         self.frame = 0
         self.action = 0
         self.face_dir = 1
@@ -40,10 +41,19 @@ class Bird:
 
         self.x += self.dir * RUN_SPEED_PPS * game_framework.frame_time
         self.x = clamp(25, self.x, 1600 - 25)
-        pass
+
+        if self.x == 1600-25 :
+            self.dir = -1
+        elif self.x == 25 :
+            self.dir = 1
+
 
     def handle_event(self, event):
         pass
 
     def draw(self):
-        self.image.clip_draw(int(self.frame) * 182, self.action * 168, 182, 163, self.x, self.y)
+        if self.dir == 1 :
+            self.image.clip_draw(int(self.frame) * 182, self.action * 168, 182, 163, self.x, self.y,BIRD_SIZE[0],BIRD_SIZE[1])
+        if self.dir == -1 :
+            self.image.clip_composite_draw(int(self.frame) * 182, self.action * 168, 182, 163,0,'h', self.x, self.y,BIRD_SIZE[0],BIRD_SIZE[1])
+
